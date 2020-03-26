2c730180e535e401fdd93099be6fea4faf39c8ef
João Guilherme Souza <joao.souza@deal.br>
2020-03-26 18:45:01 -0300

    Estrutura basica do programa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace todo_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
